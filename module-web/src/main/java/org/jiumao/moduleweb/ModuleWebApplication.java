11e593280b1c080abfd0db049ed8e881eb2e1529
xvchushun <xvchushun@gmail.com>
2024-12-01 21:39:21 +0800

    chore: 初始化web模块

---
+++ module-web/src/main/java/org/jiumao/moduleweb/ModuleWebApplication.java
@@ -0,0 +1,13 @@
+package org.jiumao.moduleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleWebApplication.class, args);
+	}
+
+}
---
+++ module-web/src/test/java/org/jiumao/moduleweb/ModuleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jiumao.moduleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
